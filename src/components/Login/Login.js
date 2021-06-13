f86ca25f0202469239c9eeef5b31298c70ff0602
DaviD D <jdavid.martinez@gmail.com>
2021-06-13 07:51:17 -0500

    Section 10 video 112

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,11 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setTimeout(()=>{
+    const identifier = setTimeout(()=>{
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
     }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
     
   }, [enteredEmail, enteredPassword]);
 
